1f75b9a98ee54d6631d8246461f5c92f2ef06ab2
ErosKoller <eroskoller.dev@gmail.com>
2025-01-15 15:48:37 -0300

    yml

---
+++ src/main/java/com/eroskoller/practicing/accounts/controller/AccountsController.java
@@ -0,0 +1,20 @@
+package com.eroskoller.practicing.accounts.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountsController {
+
+
+
+    @GetMapping("say-hello")
+    public ResponseEntity<String> getAccounts() {
+        return ResponseEntity.ok("Accounts");
+    }
+
+}
